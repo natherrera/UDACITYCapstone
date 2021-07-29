71279c4c2ee5625be7b6a875ed576f3fe3a7ddc7
natherrera <natherrera@MacBook-Air-de-Natalia.local>
2021-07-29 01:45:20 -0400

    get apis

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify 
 from .models import setup_db, Actor, Movie
-from .auth.auth import AuthError 
+from .auth.auth import AuthError, requires_auth 
 from flask_cors import CORS
+from werkzeug.exceptions import Unauthorized, BadRequest, NotFound
 
 def create_app(test_config=None):
 
@@ -32,6 +33,50 @@ def index():
 @app.route('/home', methods=['GET','POST'])
 def user_logged():
     return render_template("home.html")
+
+#----------------------------------------------------------------------------#
+# /actors API
+#----------------------------------------------------------------------------#
+
+@app.route('/actors', methods=['GET'])
+@requires_auth('view:actors')
+def get_actors(payload):
+    try:
+        actors_list = Actor.query.all()
+        actors = [actor.format() for actor in actors_list]
+        if len(actors) == 0:
+            return NotFound()
+        else:
+            response = {
+            'success': True,
+            'status_code': 200,
+            'actors' : actors ,
+            }
+            return jsonify(response)
+    except Exception:
+        raise Unauthorized()
+
+#----------------------------------------------------------------------------#
+# /movies API
+#----------------------------------------------------------------------------#
+
+@app.route('/movies', methods=['GET'])
+@requires_auth('view:movies')
+def get_movies(payload):
+    try:
+        movies_list = Movie.query.all()
+        movies = [movie.format() for movie in movies_list]
+        if len(movies) == 0:
+            return NotFound()
+        else:
+            response = {
+            'success': True,
+            'status_code': 200,
+            'movies' : movies ,
+            }
+            return jsonify(response)
+    except Exception:
+        raise Unauthorized()
 
 if __name__ == '__main__':
     app.run()
