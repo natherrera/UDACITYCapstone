017e650d090b680b52ff8b19637e488868814b37
natherrera <natherrera@MacBook-Air-de-Natalia.local>
2021-07-31 16:19:52 -0400

    fix imports

--- test_app.py
+++ test_app.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 # from datetime import date
 from app import create_app
-from models import setup_db, Actor, Movie
+from models.models import setup_db, Actor, Movie
 
 class CapstoneTestCase(unittest.TestCase):
     def setUp(self):
